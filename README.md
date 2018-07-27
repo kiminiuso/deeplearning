a9f5558c4e6fdc84c8825804973458a708751aed
xiapanan <xiapanan@supcon.com>
2018-07-27 14:39:02 +0800

    190727

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # deeplearning
 机器学习
+
+人工智能 > 机器学习 > 深度学习
+
+关键词
+
+神经网络 NN
+    激活函数:sigmoid,tanh,Softmax,ReLU,Leaky ReLU,PReLU
+    损失函数、代价函数
+    各激活函数的回归效率可以从 http://playground.tensorflow.org 直观的看出
+    优化算法
+    反向传播
+
+
+卷积神经网络CNN
+    卷积层、池化层、全连接层。conv->bn->ReLU 。感受野
+循环卷积神经网络RNN
+长短时记忆网络LSTM
---
+++ pima-indians-keras.py
@@ -0,0 +1,41 @@
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy
+# fix random seed for reproducibility
+# seed = 10
+# numpy.random.seed(seed)
+
+#加载数据
+dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
+# 区分前8个输入和第九个输出
+X = dataset[0:len(dataset), 0:8]
+Y = dataset[0:len(dataset), 8]
+
+# 创建模型，包含激活函数、输入类型
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu',kernel_initializer="uniform"))
+model.add(Dense(8, activation='relu', kernel_initializer="uniform"))
+model.add(Dense(1, activation='sigmoid', kernel_initializer="uniform"))
+# model.add(Dense(16, input_dim=8, activation='relu',kernel_initializer="uniform"))
+# model.add(Dense(12, activation='relu', kernel_initializer="uniform"))
+# model.add(Dense(8, activation='relu', kernel_initializer="uniform"))
+# model.add(Dense(1, activation='sigmoid', kernel_initializer="uniform"))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+# 开始训练
+# 手动输入训练集
+# model.train_on_batch(x_batch, y_batch)
+model.fit(X, Y, epochs=200, batch_size=10)
+
+# evaluate the model
+scores = model.evaluate(X, Y)
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+
+# calculate predictions
+predictions = model.predict(X)
+# round predictions
+rounded = [round(x) for x in predictions.ravel()]
+print(rounded)
