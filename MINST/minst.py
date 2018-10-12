18788dfe0037908f51a51649d6fce39ab2ee15f0
xiapanan <xiapanan@supcon.com>
2018-10-12 14:50:05 +0800

    cnn按照http://scs.ryerson.ca/~aharley/vis/conv/转换了一遍

--- MINST/minst.py
+++ MINST/minst.py
@@ -24,8 +24,9 @@ def bias_variable(shape):
 # 创建卷积op
 # x 是一个4维张量，shape为[batch,height,width,channels]
 # 卷积核移动步长为1。填充类型为SAME,可以不丢弃任何像素点, VALID丢弃边缘像素点
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding="SAME")
+# 感受野
+# def conv2d(x, W, padding):
+#     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding=padding)
 
 
 # 创建池化op
@@ -39,7 +40,7 @@ def max_pool_2x2(x):
 
 
 # 第1层，卷积层
-# 初始化W为[5,5,1,32]的张量，表示卷积核大小为5*5，1表示图像通道数，6表示卷积核个数即输出6个特征图
+# 初始化W为[5,5,1,6]的张量，表示卷积核大小为5*5，1表示图像通道数，6表示卷积核个数即输出6个特征图
 W_conv1 = weight_variable([5, 5, 1, 6])
 # 初始化b为[6],即输出大小
 b_conv1 = bias_variable([6])
@@ -50,7 +51,7 @@ x_image = tf.reshape(x, [-1, 28, 28, 1])
 
 # 把x_image和权重进行卷积，加上偏置项，然后应用ReLU激活函数，最后进行max_pooling
 # h_pool1的输出即为第一层网络输出，shape为[batch,14,14,6]
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_conv1 = tf.nn.relu(tf.nn.conv2d(x_image, W_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 # 第2层，卷积层
@@ -58,33 +59,40 @@ h_pool1 = max_pool_2x2(h_conv1)
 W_conv2 = weight_variable([5, 5, 6, 16])
 b_conv2 = weight_variable([16])
 
-# h_pool2即为第二层网络输出，shape为[batch,7,7,16]
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+# h_pool2即为第二层网络输出，shape为[batch,5,5,16]
+h_conv2 = tf.nn.relu(tf.nn.conv2d(h_pool1, W_conv2, strides=[1, 1, 1, 1], padding='VALID') + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
 # 第3层, 全连接层
-# 这层是拥有1024个神经元的全连接层
-# W的第1维size为7*7*16，7*7是h_pool2输出的size，16是第2层输出神经元个数
-W_fc1 = weight_variable([7 * 7 * 16, 120])
+# 这层是拥有120个神经元的全连接层
+# W的第1维size为5*5*16，5*5是h_pool2输出的size，16是第2层输出神经元个数
+W_fc1 = weight_variable([5 * 5 * 16, 120])
 b_fc1 = bias_variable([120])
 
-# 计算前需要把第2层的输出reshape成[batch, 7*7*16]的张量
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 16])
+# 计算前需要把第2层的输出reshape成[batch, 5*5*16]的张量
+h_pool2_flat = tf.reshape(h_pool2, [-1, 5 * 5 * 16])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+# 第四层全连接层
+# 120个神经元转换成100个神经元，用tanh回归
+W_fc4 = weight_variable([120, 100])
+b_fc4 = bias_variable([100])
+
+h_pool4_flat = tf.reshape(h_fc1, [-1, 120])
+h_fc4 = tf.nn.tanh(tf.matmul(h_pool4_flat, W_fc4) + b_fc4)
 
 # Dropout层
 # 为了减少过拟合，在输出层前加入dropout
 keep_prob = tf.placeholder("float")
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+h_fc1_drop = tf.nn.dropout(h_fc4, keep_prob)
 
 # 输出层
 # 最后，添加一个softmax层
 # 可以理解为另一个全连接层，只不过输出时使用softmax将网络输出值转换成了概率
-W_fc2 = weight_variable([120, 10])
-b_fc2 = bias_variable([10])
+W_out = weight_variable([100, 10])
+b_out = bias_variable([10])
 
-y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
-
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_out) + b_out)
 # 预测值和真实值之间的交叉墒
 cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))
 
